61205eb43fccdeb6792447e70700c8a066bcfbb0
GugaIsaac <linosantanapereira@gmail.com>
2024-01-28 10:20:03 -0300

    criado quadrado/tabuleiro do jogo da velha, clique neles estão funcionando

---
+++ Projeto Jogo da Velha/js/script.js
@@ -0,0 +1,48 @@
+let bloco1 = document.querySelector("#bloco1");
+let bloco2 = document.querySelector("#bloco2");
+let bloco3 = document.querySelector("#bloco3");
+let bloco4 = document.querySelector("#bloco4");
+let bloco5 = document.querySelector("#bloco5");
+let bloco6 = document.querySelector("#bloco6");
+let bloco7 = document.querySelector("#bloco7");
+let bloco8 = document.querySelector("#bloco8");
+let bloco9 = document.querySelector("#bloco9");
+
+let elementBloco1 = document.getElementById("#bloco1")
+let elementBloco2 = document.getElementById("#bloco2")
+let elementBloco3 = document.getElementById("#bloco3")
+let elementBloco4 = document.getElementById("#bloco4")
+let elementBloco5 = document.getElementById("#bloco5")
+let elementBloco6 = document.getElementById("#bloco6")
+let elementBloco7 = document.getElementById("#bloco7")
+let elementBloco8 = document.getElementById("#bloco8")
+let elementBloco9 = document.getElementById("#bloco9")
+
+bloco1.addEventListener("click", function(){
+    let texto = document.createTextNode("X")
+    elementBloco1.append(texto);//entender como faço para aparecer o texto aq
+})
+bloco2.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco3.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco4.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco5.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco6.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco7.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco8.addEventListener("click", function(){
+    alert("APERTOU ");
+})
+bloco9.addEventListener("click", function(){
+    alert("APERTOU ");
+})
